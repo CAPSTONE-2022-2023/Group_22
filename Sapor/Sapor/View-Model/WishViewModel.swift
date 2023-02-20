d6b3db509a04755a2e87e75b03c91d9f93a5cfe0
Alireza Zahedi <alireza@Alirezas-MBP.ht.home>
2023-02-20 18:47:32 -0500

    complete wishlist

--- Sapor/Sapor/View-Model/WishViewModel.swift
+++ Sapor/Sapor/View-Model/WishViewModel.swift
@@ -4,6 +4,7 @@ import FirebaseAuth
 
 class WishViewModel: ObservableObject {
     @Published var wishlist: [String] = []
+    @Published var items: [Item] = []
     
     private var db = Firestore.firestore()
     private var userId: String? {
@@ -21,7 +22,7 @@ class WishViewModel: ObservableObject {
             return
         }
         
-        let docRef = db.collection("users").document(userId)
+        let docRef = db.collection("Wishlist").document(userId)
         listenerRegistration = docRef.addSnapshotListener { [weak self] documentSnapshot, error in
             guard let document = documentSnapshot else {
                 print("Error fetching document: \(error!)")
@@ -40,7 +41,7 @@ class WishViewModel: ObservableObject {
         }
     }
     
-
+    
     func removeItem(name: String) {
         wishlist.removeAll { $0 == name }
         persistInFirebase()
@@ -60,13 +61,45 @@ class WishViewModel: ObservableObject {
             return
         }
         
-        db.collection("users").document(userId).setData(["wishlist": wishlist]) { error in
+        db.collection("Wishlist").document(userId).setData(["wishlist": wishlist]) { error in
             if let error = error {
                 print("Error writing document: \(error)")
             } else {
                 print("Document successfully written!")
             }
         }
+        
+    }
+    
+    func fetchItems() {
+        FirebaseManager.shared.firestoreDB.collection("Menu_Items").getDocuments(completion: { querySnapshot, err in
+            if let e = err {
+                print(e.localizedDescription)
+            }
+            
+            guard let documents = querySnapshot else {
+                print("No documents")
+                return;
+            }
+            self.items = []
+            documents.documents.forEach { each in
+                let name = each["name"] as! String
+                let price = each["price"] as! String
+                let category = each["category"] as! String
+                let description = each["description"] as! String
+                let imageURL = each["imageURL"] as! String
+                    for item in self.wishlist {
+                        if item == name {
+                            print("\(item) and \(name)")
+                            self.items.append(.init(name: name, price: price, description: description, category: category, imageURL: imageURL))
+                            break
+                        }
+                    }
+
+            }
+        })
+        
+        
     }
     
     deinit {
--- Sapor/Sapor/View/WishlistView.swift
+++ Sapor/Sapor/View/WishlistView.swift
@@ -9,11 +9,81 @@ import SwiftUI
 
 struct WishlistView: View {
     @ObservedObject var vm: WishViewModel = .shared
+    
+    init() {
+        print("Wishlist View")
+    }
+    
     var body: some View {
-        VStack {
-            ForEach(vm.wishlist, id: \.self) { itemName in
-                Text(itemName)
+        Group {
+            if !vm.items.isEmpty {
+                ScrollView(.vertical){
+                    VStack(spacing: 30){
+                        ForEach(self.vm.items, id: \.self) { item in
+                            HStack(){
+                                // MARK: Item Description
+                                VStack(alignment: .leading, spacing: 10){
+                                    HStack{
+                                        Text(item.name)
+                                            .font(.body)
+                                            .bold()
+                                        Spacer()
+                                        let has = vm.wishlist.contains { str in
+                                            str == item.name;
+                                        };
+                                        Button {
+                                            if !has {
+                                                vm.addItem(name: item.name)
+                                            } else {
+                                                vm.removeItem(name: item.name)
+                                                vm.items.removeAll { i in
+                                                    i.name == item.name
+                                                }
+                                            }
+                                        } label: {
+                                            Image(systemName: has ? "heart.fill" : "heart").foregroundColor(.orange)
+                                        }
+                                        
+                                        Button {
+                                            // TODO: add to basket
+                                            //                                    menu.addToBasket(item:item)
+                                        } label: {
+                                            Image(systemName: "cart.fill.badge.plus").foregroundColor(.orange)
+                                        }
+                                        
+                                    }
+                                    Text("$\(item.price)")
+                                        .font(.subheadline)
+                                    
+                                    Text(item.description)
+                                        .font(.callout)
+                                        .foregroundColor(.secondary)
+                                }
+                                
+                                Spacer()
+                                
+                                // MARK: Item Image
+                                AsyncImage(url: URL(string: item.imageURL)) { image in
+                                    image.resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: 100, height: 100)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
+                            .padding(.horizontal)
+                        }
+                    }
+                }
+            } else {
+                Text("When you like an item it will be displayed here")
+                    .foregroundColor(.secondary)
             }
+        }
+        .onAppear {
+            print("ITEMS")
+            vm.fetchItems()
         }
     }
 }
